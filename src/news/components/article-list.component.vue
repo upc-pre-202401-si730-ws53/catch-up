0dfb20ea92040f6a8448e6f962efbd8cc5dfaa24
Angel Velasquez <pcisavel@upc.edu.pe>
2024-04-11 16:41:54 -0500

    feat(article-list): implemented article list section components.

---
+++ src/news/components/article-list.component.vue
@@ -0,0 +1,17 @@
+<script>
+
+
+import ArticleCard from "./article-card.component.vue";
+
+export default {
+  name: "article-list",
+  components: {ArticleCard},
+  props: { articles: Array }
+}
+</script>
+
+<template>
+  <div v-for="article in articles">
+    <article-card :article="article"/>
+  </div>
+</template>
